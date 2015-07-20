56285525a33db35060536a3d67b2fbfbf228c1b1
Jonathan <j.marshall@motherhosting.com>
2015-07-20 21:19:59 +0100

    I change blah blah blah

--- WindowsFormsApplication1/addclient.Designer.cs
+++ WindowsFormsApplication1/addclient.Designer.cs
@@ -86,9 +86,9 @@
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(12, 24);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(147, 24);
+            this.label1.Size = new System.Drawing.Size(185, 24);
             this.label1.TabIndex = 2;
-            this.label1.Text = "Add Client Form";
+            this.label1.Text = "Speedy is awesome!\r\n";
             // 
             // LabelFirstName
             // 
